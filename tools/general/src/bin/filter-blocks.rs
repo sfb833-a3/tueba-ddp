f4edd269a8139673aaf69437b92a30c5d21bcd96
Daniël de Kok <me@danieldk.eu>
2019-07-10 10:51:37 +0200

    Add --html option to filter blocks containing HTML

--- tools/general/src/bin/filter-blocks.rs
+++ tools/general/src/bin/filter-blocks.rs
@@ -2,15 +2,34 @@ use std::char::REPLACEMENT_CHARACTER;
 use std::io::{BufRead, BufWriter, Write};
 
 use clap::{App, AppSettings, Arg, ArgMatches};
+use lazy_static::lazy_static;
+use regex::{Regex, RegexSet};
 use stdinout::{Input, OrExit, Output};
 
 static INPUT: &'static str = "INPUT";
 static OUTPUT: &'static str = "OUTPUT";
+static HTML: &'static str = "HTML";
 static REPLACEMENT: &'static str = "REPLACEMENT";
+
+lazy_static! {
+    static ref BR_RE: Regex = Regex::new("<br */?>").unwrap();
+    static ref HTML_TAG_RE: RegexSet = RegexSet::new(&[
+        "<[^>]+/>",
+        "<[[:alnum:]:]+>",
+        "<[[:alnum:]:]+ [^>]+=[^>]+>",
+        "</[[:alnum:]]"
+    ])
+    .unwrap();
+}
 
 fn parse_args() -> ArgMatches<'static> {
     App::new("filter-blocks")
         .settings(DEFAULT_CLAP_SETTINGS)
+        .arg(
+            Arg::with_name(HTML)
+                .long("html")
+                .help("Remove br tags, remove blocks with HTML"),
+        )
         .arg(
             Arg::with_name(REPLACEMENT)
                 .long("replacement")
@@ -22,17 +41,20 @@ fn parse_args() -> ArgMatches<'static> {
 }
 
 struct Config {
+    html: bool,
     input: Option<String>,
     output: Option<String>,
     replacement: bool,
 }
 
 fn config_from_matches<'a>(matches: &ArgMatches<'a>) -> Config {
+    let html = matches.is_present(HTML);
     let input = matches.value_of(INPUT).map(ToOwned::to_owned);
     let output = matches.value_of(OUTPUT).map(ToOwned::to_owned);
     let replacement = matches.is_present(REPLACEMENT);
 
     Config {
+        html,
         input,
         output,
         replacement,
@@ -55,10 +77,18 @@ fn main() {
     let mut writer = BufWriter::new(output.write().or_exit("Cannot open output for writing", 1));
 
     for line in reader.lines() {
-        let line = line.or_exit("Cannot read line", 1);
+        let mut line = line.or_exit("Cannot read line", 1);
 
         if config.replacement && line.contains(REPLACEMENT_CHARACTER) {
             continue;
+        }
+
+        if config.html {
+            line = BR_RE.replace_all(&line, " ").into_owned();
+
+            if HTML_TAG_RE.is_match(&line) {
+                continue;
+            }
         }
 
         writeln!(&mut writer, "{}", line).or_exit("Cannot write block", 1);
