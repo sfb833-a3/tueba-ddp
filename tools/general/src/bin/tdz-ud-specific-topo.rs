c35131e6306918756a07be0acb4641e849c571bb
Daniël de Kok <me@danieldk.eu>
2020-04-17 12:06:01 +0200

    Add tdz-ud-specific-topo
    This utility picks the most specific topological field from the UD
    TopoField feature. E.g. for a token that is in the middle field of a
    final field,
    
    NF-MF
    
    is replaced by
    
    MF

---
+++ tools/general/src/bin/tdz-ud-specific-topo.rs
@@ -0,0 +1,57 @@
+use std::io::BufWriter;
+
+use clap::{App, AppSettings, Arg};
+use conllu::io::{ReadSentence, Reader, WriteSentence, Writer};
+use conllu::token::Misc;
+use stdinout::{Input, OrExit, Output};
+
+static DEFAULT_CLAP_SETTINGS: &[AppSettings] = &[
+    AppSettings::DontCollapseArgsInUsage,
+    AppSettings::UnifiedHelpMessage,
+];
+
+fn main() {
+    let matches = App::new("tdz-ud-specific-topo")
+        .settings(DEFAULT_CLAP_SETTINGS)
+        .arg(Arg::with_name("INPUT").help("Input file").index(1))
+        .arg(Arg::with_name("OUTPUT").help("Output file").index(2))
+        .get_matches();
+
+    let input = Input::from(matches.value_of("INPUT"));
+    let reader = Reader::new(input.buf_read().or_exit("Cannot open input", 1));
+
+    let output = Output::from(matches.value_of("OUTPUT"));
+    let mut writer = Writer::new(BufWriter::new(
+        output.write().or_exit("Cannot create output", 1),
+    ));
+
+    for sentence in reader.sentences() {
+        let mut sentence = sentence.or_exit("Cannot read sentence", 1);
+
+        for node in sentence.iter_mut() {
+            if let Some(token) = node.token_mut() {
+                if token.misc().is_none() {
+                    token.set_misc(Some(Misc::new()));
+                }
+
+                let misc = token.misc_mut().unwrap();
+
+                if let Some(topo) = misc.get_mut("TopoField") {
+                    let topo = topo
+                        .as_mut()
+                        .or_exit("Topological field feature without value: {}", 1);
+                    let most_specific_topo = topo.split('-').last().or_exit(
+                        format!("Incomplete topological field annotation: {}", topo),
+                        1,
+                    );
+
+                    *topo = most_specific_topo.to_owned();
+                }
+            }
+        }
+
+        writer
+            .write_sentence(&sentence)
+            .or_exit("Cannot write sentence", 1);
+    }
+}
