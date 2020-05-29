d434536bf74fef307508d0a7d1c29c9b28118093
Daniël de Kok <me@danieldk.eu>
2020-05-29 09:37:40 +0200

    political-speeches: update shell.nix

--- tools/political-speeches/shell.nix
+++ tools/political-speeches/shell.nix
@@ -1,16 +1,10 @@
-with import <nixpkgs> {};
+with import (import nix/sources.nix).nixpkgs {};
 
 let
-  danieldk = pkgs.callPackage (builtins.fetchTarball {
-    url = "https://git.sr.ht/~danieldk/nix-packages/archive/709c93a84504d558613bfc2538297ef2c532b890.tar.gz";
-    sha256 = "0jspqxz8yzghn4j3awiqz3f76my8slk3s5ckk3gfzvhq1p0wzp5m";
-  }) {};
-in stdenv.mkDerivation rec {
-  name = "political-speeches-env";
-  env = buildEnv { name = name; paths = buildInputs; };
-
-  nativeBuildInputs = [
-  ];
+  sources = import nix/sources.nix;
+  danieldk = callPackage sources.danieldk {};
+in mkShell {
+  nativeBuildInputs = [];
 
   buildInputs = [];
 
