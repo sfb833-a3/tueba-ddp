4af53586cf4f97f1c7ca93476f9a9b4cd8c9be2e
Daniël de Kok <me@danieldk.eu>
2020-05-29 14:16:50 +0200

    political-speeches: add metadata in the form of CoNLL-U comments

--- tools/political-speeches/tokenize.py
+++ tools/political-speeches/tokenize.py
@@ -1,43 +1,93 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import hashlib
+from optparse import OptionParser
+
 from lxml import etree
 import somajo
 
-from optparse import OptionParser
+def raw_sentence(tokens):
+    tokensWithSpaces = []
+    for token in tokens:
+        tokensWithSpaces.append(str(token))
+        if token.space_after:
+            tokensWithSpaces.append(' ')
+    return ''.join(tokensWithSpaces).strip()
 
 if __name__ == "__main__":
-    usage = "usage: %prog [options] corpus.xml corpus.conll sent_doc_ids"
+    usage = "usage: %prog [options] corpus.xml corpus.conll"
     parser = OptionParser(usage)
 
     (options, args) = parser.parse_args()
 
-    if len(args) != 3:
+    if len(args) != 2:
         parser.error("incorrect number of arguments")
 
-    tokenizer = somajo.Tokenizer()
-    splitter = somajo.SentenceSplitter(is_tuple=False)
+    tokenizer = somajo.SoMaJo("de_CMC")
 
-    with open(args[0]) as inputCorpus, open(args[1], "w") as outputCorpus, open(args[2], "w") as sentDocIds:
+    with open(args[0]) as inputCorpus, open(args[1], "w") as outputCorpus:
         tree = etree.parse(inputCorpus)
         
         for text in tree.iter("text"):
             url = text.attrib["url"]
 
-            anrede = text.attrib["anrede"]
+            title = text.attrib["titel"]
+            subtitle = text.attrib["untertitel"]
+            person = text.attrib["person"]
+            date = text.attrib["datum"]
+            # Use default value, 'ort' misses in various documents.
+            location = text.attrib.get("ort", None)
+            url = text.attrib["url"]
+            salutation = text.attrib["anrede"]
+
+            if not title:
+                raise RuntimeError("Text without a title?")
+
+            # URLs are to inconvenient
+            if url:
+                docId = "political-speeches-{}".format(hashlib.sha512(url.encode("utf-8")).hexdigest()[:10])
+            else:
+                docId = "political-speeches-{}".format(hashlib.sha512(title.encode("utf-8")).hexdigest()[:10])
+
+            outputCorpus.write(f"# newdoc id = {docId}\n")
+            if url:
+                outputCorpus.write(f"# url = {url}\n");
+            if title:
+                outputCorpus.write(f"# title = {title}\n");
+            if subtitle:
+                outputCorpus.write(f"# subtitle = {subtitle}\n");
+            if person:
+                outputCorpus.write(f"# person = {person}\n");
+            if date:
+                outputCorpus.write(f"# date = {date}\n");
+            if location:
+                outputCorpus.write(f"# location = {location}\n");
+            if salutation:
+                outputCorpus.write(f"# salutation = {salutation}\n");
 
             # Text always has one child: rohtext
             rawText = text[0].text
+            nPara = 0
 
-            tokens = tokenizer.tokenize(rawText)
-            sents = splitter.split(tokens)
+            for para in rawText.splitlines():
+                if not para.strip():
+                    continue
 
-            for sent in sents:
-                counter = 1
-                for token in sent:
-                    outputCorpus.write("%d\t%s\n" % (counter, token))
-                    counter += 1
+                outputCorpus.write(f"# newpar id = {docId}-{nPara}\n")
 
-                outputCorpus.write("\n")
+                sents = tokenizer.tokenize_text([para])
+                nSents = 0
 
-                sentDocIds.write("%s\n" % url)
+                for sent in sents:
+                    outputCorpus.write("# text = {}\n".format(raw_sentence(sent)))
+                    outputCorpus.write(f"# sent_id = {docId}-{nPara}-{nSents}\n")
+                    counter = 1
+                    for token in sent:
+                        outputCorpus.write("%d\t%s\n" % (counter, token))
+                        counter += 1
+
+                    outputCorpus.write("\n")
+                    nSents += 1
+
+                nPara += 1
